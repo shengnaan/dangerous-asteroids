41caf1cb16dc6e61a2fb6a07b38f074579cf068a
shengnan <pelevin1111@gmail.com>
2024-12-12 05:10:08 +0300

    rename

--- src/utils/resources.py
+++ src/utils/resources.py
@@ -31,16 +31,16 @@ img_explosion = pygame.image.load(os.path.join(Settings.IMAGES_DIR, "explosion.p
 icon = pygame.image.load(os.path.join(Settings.IMAGES_DIR, "icon.png"))
 
 
-mscBackgroundTheme = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, 'mscBackgroundTheme.wav'))
-mscBackgroundTheme.set_volume(0.2)
-if mscBackgroundTheme:
-    mscBackgroundTheme.play(100)
+msc_background_theme = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, 'mscBackgroundTheme.wav'))
+msc_background_theme.set_volume(0.2)
+if msc_background_theme:
+    msc_background_theme.play(100)
 
-mscSpaceRocketThrust = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, "mscSpaceRocketThrust.wav"))
-mscSpaceRocketThrust.set_volume(0.9)
+msc_rocket_thrust = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, "mscRocketThrust.wav"))
+msc_rocket_thrust.set_volume(0.9)
 
-mscMissileShot = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, 'mscMissileShot.wav'))
-mscMissileShot.set_volume(0.2)
+msc_missile_shot = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, 'mscMissileShot.wav'))
+msc_missile_shot.set_volume(0.2)
 
-mscExplosion = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, "mscExplosion.wav"))
-mscExplosion.set_volume(0.8)
+msc_explosion = pygame.mixer.Sound(os.path.join(Settings.SOUNDS_DIR, "mscExplosion.wav"))
+msc_explosion.set_volume(0.8)
