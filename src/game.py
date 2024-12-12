f7b8243a7724cdd4f48fabae7abc225a386bf3be
shengnan <pelevin1111@gmail.com>
2024-12-12 04:12:20 +0300

    rename img

--- src/game.py
+++ src/game.py
@@ -1,20 +1,19 @@
-
-
 import pygame
 
 from config import Settings
 from src.assets.fonts.fonts_info import font
-from src.assets.images.img_info import (imgInfoSpaceRocket, imgSpaceRocketNoThrust, imgSpaceRocketThrust, imgSpaceWorld,
-                                        imgIntroUltimateAsteroids, imgInfoIntroUltimateAsteroids)
+from src.assets.images.img_info import (img_info_rocket, img_rocket, img_accelerating_rocket, img_background,
+                                        img_intro, img_info_intro)
 from src.assets.rocket import SpaceRocket
 from src.utils.helpers import asteroid_spawner, group_collide, process_sprite_group, group_group_collide, draw_on_screen
+
 def game():
 
     LIVES = Settings.LIVES
     SCORE = Settings.SCORE
     running = True
-    spaceRocket = SpaceRocket([Settings.WIDTH / 2, Settings.HEIGHT / 2], [0, 0], 0, imgSpaceRocketNoThrust, imgSpaceRocketThrust,
-                              imgInfoSpaceRocket)
+    spaceRocket = SpaceRocket([Settings.WIDTH / 2, Settings.HEIGHT / 2], [0, 0], 0, img_rocket, img_accelerating_rocket,
+                              img_info_rocket)
     asteroids_group = set([])
     missile_group = set([])
     explosion_group = set([])
@@ -23,8 +22,13 @@ def game():
     pygame.time.set_timer(ROCK_SPAWN, 1000)
 
     screen = pygame.display.set_mode((Settings.WIDTH, Settings.HEIGHT))
+
+    draw_on_screen(screen, img_intro, img_info_intro.get_center(),
+                   img_info_intro.get_size(), [Settings.WIDTH / 2, Settings.HEIGHT / 2],
+                   img_info_intro.get_size())
+
     while running:
-        screen.blit(imgSpaceWorld, (0, 0))
+        screen.blit(img_background, (0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,16 +72,16 @@ def game():
         spaceRocket.update()
 
         if LIVES <= 0:
-            draw_on_screen(screen, imgIntroUltimateAsteroids, imgInfoIntroUltimateAsteroids.get_center(),
-                           imgInfoIntroUltimateAsteroids.get_size(), [Settings.WIDTH / 2, Settings.HEIGHT / 2],
-                           imgInfoIntroUltimateAsteroids.get_size())
+            draw_on_screen(screen, img_intro, img_info_intro.get_center(),
+                           img_info_intro.get_size(), [Settings.WIDTH / 2, Settings.HEIGHT / 2],
+                           img_info_intro.get_size())
 
             for asteroid in list(asteroids_group):
                 asteroids_group.remove(asteroid)
 
         score_render = font.render("Score: " + str(SCORE), True, (255, 255, 255))
-        screen.blit(score_render, (10, 10))
+        screen.blit(score_render, (10, 15))
         lives_render = font.render("Lives: " + str(LIVES), True, (255, 255, 255))
-        screen.blit(lives_render, (685, 10))
+        screen.blit(lives_render, (1660, 15))
 
         pygame.display.update()
